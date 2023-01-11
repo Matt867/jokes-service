e7918699440b23eb2e9d3a5676ea15806f821478
Matthew Sidaway <matthewsidaway@outlook.com>
2023-01-11 13:08:44 +0000

    jokes endpoint made

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const { Joke } = require('./db');
+const { Sequelize, Op } = require("sequelize");
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
@@ -8,7 +9,30 @@ app.use(express.urlencoded({extended:true}));
 app.get('/jokes', async (req, res, next) => {
   try {
     // TODO - filter the jokes by tags and content
-    const jokes = [];
+    const {tags, content} = req.query;
+
+    if (!tags && !content) {
+      const jokes = await Joke.findAll();
+      res.json(jokes);
+    }
+
+    const jokes = await Joke.findAll({
+      where: {
+        [Op.or]: [
+          {
+            tags : {
+            [Op.like]: '%' + tags + '%'   
+          }
+          },
+          {
+            joke: {
+              [Op.like]: '%' + content + '%'
+            }
+          }
+        ]
+      }
+    })
+
     res.send(jokes);
   } catch (error) {
     console.error(error);
--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const { sequelize } = require('./db');
 
 const { PORT = 4000 } = process.env;
 
+app.get('/cars', (req, res) => {
+
+})
+
+
 app.listen(PORT, () => {
   sequelize.sync({ force: false });
   console.log(`Jokes are ready at http://localhost:${PORT}`);
